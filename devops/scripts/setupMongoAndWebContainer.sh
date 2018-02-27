68b4652bd831b3f2b08e2acf5055fe3d348b8783
Yashwant Mahawar <yashwant.mahawar@intimetec.com>
2018-02-27 10:03:29 +0530

    workspace added

--- devops/scripts/setupMongoAndWebContainer.sh
+++ devops/scripts/setupMongoAndWebContainer.sh
@@ -8,21 +8,21 @@ mongoContainerTag=$2
 echo "required web container tag $webContainerTag"
 echo "required mongo container tag $mongoContainerTag"
 isContainerRunning=$(docker ps -q -f "name=adpq-web")
-if [[ "$isContainerRunning" != "" ]]
+if [ "$isContainerRunning" != "" ]
 then
         docker stop $isContainerRunning
         docker rm $isContainerRunning
 fi
-docker build --build-arg tag=$webContainerTag -f devops/dockerFiles/web/dockerfile .
-docker build --rm --pull=true --build-arg tag=$webContainerTag -t yashittdocker/adpq-web -f devops/dockerFiles/web/dockerfile .
+docker build --build-arg tag=$webContainerTag -f ${WORKSPACE}/devops/dockerFiles/web/dockerfile .
+docker build --rm --pull=true --build-arg tag=$webContainerTag -t yashittdocker/adpq-web -f ${WORKSPACE}/devops/dockerFiles/web/dockerfile .
 docker run -d --name adpq-web -p 80:8080 yashittdocker/adpq-web:$webContainerTag
 
 isContainerRunning=$(docker ps -q -f "name=adpq-mongo")
-if [[ "$isContainerRunning" != "" ]]
+if [ "$isContainerRunning" != "" ]
 then
         docker stop $isContainerRunning
         docker rm $isContainerRunning
 fi
-docker build --build-arg tag=$mongoContainerTag -f devops/dockerFiles/mongo/dockerfile .
-docker build --rm --pull=true --build-arg tag=$mongoContainerTag -t yashittdocker/adpq-mongo -f devops/dockerFiles/mongo/dockerfile .
+docker build --build-arg tag=$mongoContainerTag -f ${WORKSPACE}/devops/dockerFiles/mongo/dockerfile .
+docker build --rm --pull=true --build-arg tag=$mongoContainerTag -t yashittdocker/adpq-mongo -f ${WORKSPACE}/devops/dockerFiles/mongo/dockerfile .
 docker run -d --name adpq-mongo -p 27017:27017 yashittdocker/adpq-mongo:$mongoContainerTag
