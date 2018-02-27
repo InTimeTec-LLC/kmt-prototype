ae819bba11d7f1148a16f941e01db53cae982c12
Yashwant Mahawar <yashwant.mahawar@intimetec.com>
2018-02-27 09:42:11 +0530

    sudo from commds removed

--- devops/scripts/setupMongoAndWebContainer.sh
+++ devops/scripts/setupMongoAndWebContainer.sh
@@ -7,22 +7,22 @@ webContainerTag=$1
 mongoContainerTag=$2
 echo "required web container tag $webContainerTag"
 echo "required mongo container tag $mongoContainerTag"
-isContainerRunning=$(sudo docker ps -q -f "name=adpq-web")
+isContainerRunning=$(docker ps -q -f "name=adpq-web")
 if [[ "$isContainerRunning" != "" ]]
 then
-        sudo docker stop $isContainerRunning
-        sudo docker rm $isContainerRunning
+        docker stop $isContainerRunning
+        docker rm $isContainerRunning
 fi
-sudo docker build --build-arg tag=$webContainerTag -f ${WORKSPACE}/devops/dockerFiles/web/dockerfile .
-sudo docker build --rm --pull=true --build-arg tag=$webContainerTag -t yashittdocker/adpq-web -f ${WORKSPACE}/devops/dockerFiles/web/dockerfile .
-sudo docker run -d --name adpq-web -p 80:8080 yashittdocker/adpq-web:$webContainerTag
+docker build --build-arg tag=$webContainerTag -f ${WORKSPACE}/devops/dockerFiles/web/dockerfile .
+docker build --rm --pull=true --build-arg tag=$webContainerTag -t yashittdocker/adpq-web -f ${WORKSPACE}/devops/dockerFiles/web/dockerfile .
+docker run -d --name adpq-web -p 80:8080 yashittdocker/adpq-web:$webContainerTag
 
-isContainerRunning=$(sudo docker ps -q -f "name=adpq-mongo")
+isContainerRunning=$(docker ps -q -f "name=adpq-mongo")
 if [[ "$isContainerRunning" != "" ]]
 then
-        sudo docker stop $isContainerRunning
-        sudo docker rm $isContainerRunning
+        docker stop $isContainerRunning
+        docker rm $isContainerRunning
 fi
-sudo docker build --build-arg tag=$mongoContainerTag -f ${WORKSPACE}/devops/dockerFiles/mongo/dockerfile .
-sudo docker build --rm --pull=true --build-arg tag=$mongoContainerTag -t yashittdocker/adpq-mongo -f ${WORKSPACE}/devops/dockerFiles/mongo/dockerfile .
-sudo docker run -d --name adpq-mongo -p 27017:27017 yashittdocker/adpq-mongo:$mongoContainerTag
+docker build --build-arg tag=$mongoContainerTag -f ${WORKSPACE}/devops/dockerFiles/mongo/dockerfile .
+docker build --rm --pull=true --build-arg tag=$mongoContainerTag -t yashittdocker/adpq-mongo -f ${WORKSPACE}/devops/dockerFiles/mongo/dockerfile .
+docker run -d --name adpq-mongo -p 27017:27017 yashittdocker/adpq-mongo:$mongoContainerTag
