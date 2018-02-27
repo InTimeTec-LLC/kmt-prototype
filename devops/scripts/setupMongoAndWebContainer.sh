f729715c794d1a6b00e1181b994aa6c64c59d268
Yashwant Mahawar <yashwant.mahawar@intimetec.com>
2018-02-27 09:37:32 +0530

    folder changed to

--- devops/scripts/setupMongoAndWebContainer.sh
+++ devops/scripts/setupMongoAndWebContainer.sh
@@ -13,8 +13,8 @@ then
         sudo docker stop $isContainerRunning
         sudo docker rm $isContainerRunning
 fi
-sudo docker build --build-arg tag=$webContainerTag -f /home/ubuntu/dockerFiles/web/dockerfile .
-sudo docker build --rm --pull=true --build-arg tag=$webContainerTag -t yashittdocker/adpq-web -f /home/ubuntu/dockerFiles/web/dockerfile .
+sudo docker build --build-arg tag=$webContainerTag -f ${WORKSPACE}/devops/dockerFiles/web/dockerfile .
+sudo docker build --rm --pull=true --build-arg tag=$webContainerTag -t yashittdocker/adpq-web -f ${WORKSPACE}/devops/dockerFiles/web/dockerfile .
 sudo docker run -d --name adpq-web -p 80:8080 yashittdocker/adpq-web:$webContainerTag
 
 isContainerRunning=$(sudo docker ps -q -f "name=adpq-mongo")
@@ -23,6 +23,6 @@ then
         sudo docker stop $isContainerRunning
         sudo docker rm $isContainerRunning
 fi
-sudo docker build --build-arg tag=$mongoContainerTag -f /home/ubuntu/dockerFiles/mongo/dockerfile .
-sudo docker build --rm --pull=true --build-arg tag=$mongoContainerTag -t yashittdocker/adpq-mongo -f /home/ubuntu/dockerFiles/mongo/dockerfile .
+sudo docker build --build-arg tag=$mongoContainerTag -f ${WORKSPACE}/devops/dockerFiles/mongo/dockerfile .
+sudo docker build --rm --pull=true --build-arg tag=$mongoContainerTag -t yashittdocker/adpq-mongo -f ${WORKSPACE}/devops/dockerFiles/mongo/dockerfile .
 sudo docker run -d --name adpq-mongo -p 27017:27017 yashittdocker/adpq-mongo:$mongoContainerTag
