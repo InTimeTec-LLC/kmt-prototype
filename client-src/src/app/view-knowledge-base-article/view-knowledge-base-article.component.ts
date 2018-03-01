b55a53ad2b05838665cb31c11d7efe1b1f4e5c6a
Parul Sharma <parul.sharma@intimetec.americas>
2018-03-01 11:47:32 +0530

    created angular component of KZ view

---
+++ client-src/src/app/view-knowledge-base-article/view-knowledge-base-article.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-knowledge-base-article',
+  templateUrl: './view-knowledge-base-article.component.html',
+  styleUrls: ['./view-knowledge-base-article.component.scss']
+})
+export class ViewKnowledgeBaseArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
