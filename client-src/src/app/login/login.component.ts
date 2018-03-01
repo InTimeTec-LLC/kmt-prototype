d00180df401ad21feff124de982a74bc439c362b
Harish Patidar <harish.patidar@intimetec.com>
2018-03-01 10:41:18 +0530

    Removed href from userlist screen.

--- client-src/src/app/login/login.component.ts
+++ client-src/src/app/login/login.component.ts
@@ -24,8 +24,7 @@ export class LoginComponent implements OnInit {
       }
 
     ngOnInit() {
-        // reset login status
-        // this.authenticationService.logout();
+       
     }
 
     login() {
