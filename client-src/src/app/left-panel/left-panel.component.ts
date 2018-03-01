1f4029e6555a554f36522191c5ffc547ad6ff9ec
Harish Patidar <harish.patidar@intimetec.com>
2018-03-01 09:19:49 +0530

    Added message service for sharing the data between components.

--- client-src/src/app/left-panel/left-panel.component.ts
+++ client-src/src/app/left-panel/left-panel.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../shared/service/authentication/authentication.service';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+ 
+import { MessageService } from '../../shared/service/message/message';
+
 
 @Component({
   selector: 'app-left-panel',
@@ -10,12 +14,26 @@ import { Router } from '@angular/router';
 export class LeftPanelComponent implements OnInit {
 
   userType : any;
-  constructor(private auth: AuthenticationService, private router: Router) { 
+  subscription: Subscription;
+
+  constructor(private auth: AuthenticationService, private router: Router,private messageService: MessageService) { 
      this.userType = this.auth.getUserType(); 
+     this.subscription = this.messageService.getMessage().subscribe(message => { 
+        this.userType = this.auth.getUserType();
+     });
+  }
+
+  ngOnDestroy() {
+      // unsubscribe to ensure no memory leaks
+      this.subscription.unsubscribe();
   }
 
   ngOnInit() {
     
+  }
+
+  refreshUserType() {
+    this.userType = this.auth.getUserType();
   }
 
   onTapNavigation(route) {
--- client-src/src/app/login/login.component.ts
+++ client-src/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../shared/service/authentication/authentication.service';
 import {ToasterModule, ToasterService} from 'angular5-toaster';
+import { MessageService } from '../../shared/service/message/message';
 
 
 @Component({
@@ -17,6 +18,7 @@ export class LoginComponent implements OnInit {
   constructor(
       private router: Router, 
       private toasterService: ToasterService,
+      private messageService: MessageService,
       private authenticationService: AuthenticationService) { 
 
       }
@@ -32,6 +34,7 @@ export class LoginComponent implements OnInit {
             .subscribe(
                 data => {
                     console.log(data);
+                    this.messageService.sendMessage('set user type');
                     this.router.navigate(['/dashboard']);
                 },
                 error => {
