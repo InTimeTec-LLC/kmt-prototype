3d184390a0113eff9186bf8e387e623508dfce86
Harish Patidar <harish.patidar@intimetec.com>
2018-03-01 09:53:46 +0530

    Removed unwanted hrefs from left menu.

--- client-src/src/app/login/login.component.ts
+++ client-src/src/app/login/login.component.ts
@@ -25,7 +25,7 @@ export class LoginComponent implements OnInit {
 
     ngOnInit() {
         // reset login status
-        this.authenticationService.logout();
+        // this.authenticationService.logout();
     }
 
     login() {
