634195f0d2dbeaf6843208d747cf67499013c9c7
Simarpreet Gujral <simarpreet.gujral@intimetec.americas>
2018-03-01 11:26:35 +0530

    changed port for jenkins and web container

--- devops/scripts/setupMongoAndWebContainer.sh
+++ devops/scripts/setupMongoAndWebContainer.sh
@@ -13,7 +13,7 @@ then
 fi
 docker build --build-arg tag=$webContainerTag -f ${WORKSPACE}/devops/dockerFiles/web/dockerfile .
 docker build --rm --pull=true --build-arg tag=$webContainerTag -t yashittdocker/adpq-web -f ${WORKSPACE}/devops/dockerFiles/web/dockerfile .
-docker run -d --name adpq-web -p 80:8080 -v /home/ubuntu/tomcatDeployFiles:/usr/local/tomcat/webapps/ yashittdocker/adpq-web:$webContainerTag
+docker run -d --name adpq-web -p 8080:8080 -v /home/ubuntu/tomcatDeployFiles:/usr/local/tomcat/webapps/ yashittdocker/adpq-web:$webContainerTag
 
 isContainerRunning=$(docker ps -q -f "name=adpq-mongo")
 if [ "$isContainerRunning" != "" ]
