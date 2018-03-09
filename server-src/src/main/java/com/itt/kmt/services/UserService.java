d80feaa745b72794cef8f7263762e0d6b7a84625
RakshitRajeevintimetec <32503697+RakshitRajeevintimetec@users.noreply.github.com>
2018-03-09 10:44:00 +0530

    ADPQVP2018-214 Implement pagination , search in user list (#45)

--- server-src/src/main/java/com/itt/kmt/services/UserService.java
+++ server-src/src/main/java/com/itt/kmt/services/UserService.java
@@ -1,14 +1,16 @@
 package com.itt.kmt.services;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.mail.MailException;
 
 import com.itt.kmt.jwt.JWTUtil;
 import com.itt.kmt.models.Role;
@@ -16,9 +18,11 @@ import com.itt.kmt.models.User;
 import com.itt.kmt.repositories.RoleRepository;
 import com.itt.kmt.repositories.UserRepository;
 import com.itt.kmt.validators.UserValidator;
+import com.itt.utility.Constants;
 import com.itt.utility.EmailConstants;
 
 import lombok.extern.slf4j.Slf4j;
+
 
 /**
  * Service class that acts as an intermediary between controller and the
@@ -74,15 +78,92 @@ public class UserService {
     /**
      * Gets all the active admins and managers.
      * @param roles roles by which active users are retrieved.
+     * @param active status of users to be retrieved.
      * @return List of all the users.
      */
-    public List<User> getAllActiveUsersByRoles(final List<String> roles) {
-        List<User> listOfActiveUsersByRoles = new ArrayList<User>();
+    public List<User> getAllUsersByRolesAndStatus(final List<String> roles, final boolean active) {
+        List<User> listOfUsersByRolesAndStatus = new ArrayList<User>();
         for (String role : roles) {
-            List<User> users = repository.findByUserRole(role, true);
-            listOfActiveUsersByRoles.addAll(users);
+            List<User> users = repository.findByUserRoleAndActive(role, active);
+            listOfUsersByRolesAndStatus.addAll(users);
         }
-        return listOfActiveUsersByRoles;
+        return listOfUsersByRolesAndStatus;
+    }
+    /**
+     * Gets all the active admins and managers.
+     * @param roles roles by which active users are retrieved.
+     * @return List of all the users.
+     */
+    public List<User> getAllUsersByRoles(final List<String> roles) {
+        List<User> listOfUsersByRoles = new ArrayList<User>();
+        for (String role : roles) {
+            List<User> users = repository.findByUserRole(role);
+            listOfUsersByRoles.addAll(users);
+        }
+        return listOfUsersByRoles;
+    }
+    /**
+     * Gets all the Users by attribute passed.
+     * @param userAttribute userAttribute by which active users are retrieved.
+     * @return List of all the users.
+     */
+    public List<User> searchUsersByUserAttribute(final String userAttribute) {
+
+        return repository.findByFirstNameOrLastNameOrEmail(userAttribute);
+    }
+    /**
+     * Gets all the Users by attributes passed.
+     * @param search parameter by which users are filtered.
+     * @param role role by which active users are retrieved.
+     * @param status status by which active users are retrieved.
+     * @return List of all the users.
+     */
+    public List<User> filterUsersByStatusAndRole(final String search, final String role, final Boolean status) {
+
+        List<User> users = null;
+        List<String> roles = new ArrayList<String>();
+        roles.add(role);
+
+        if (role == null && status == null && search == null) {
+
+            users = getAllUsers();
+
+        } else if (role != null && search == null) {
+
+            if (status != null) {
+               users = getAllUsersByRolesAndStatus(roles, status);
+            } else {
+               users = getAllUsersByRoles(roles);
+            }
+        } else if (status != null && search == null) {
+            users =  repository.findByActive(status);
+
+        } else if (search != null) {
+            users = repository.findByFirstNameOrLastNameOrEmail(search);
+            List<User> filteredUsers = new ArrayList<User>();
+            for (User user : users) {
+                if (role != null && status != null) {
+
+                    if (user.getUserRole().equals(role) && user.isActive() == status) {
+                        filteredUsers.add(user);
+                    }
+                } else if (role == null && status != null) {
+                    if (user.isActive() == status) {
+                        filteredUsers.add(user);
+                    }
+                } else if (role != null && status == null) {
+                    if (user.getUserRole().equals(role)) {
+                        filteredUsers.add(user);
+                    }
+                } else {
+                    filteredUsers = users;
+                    break;
+                }
+            }
+            users = filteredUsers;
+
+        }
+        return users;
     }
     /**
      * Saves the User.
@@ -172,6 +253,7 @@ public class UserService {
         }
         return user;
     }
+
     
     /**
      * Validate user.
@@ -217,4 +299,33 @@ public class UserService {
              throw new RuntimeException("user doesnot exist");
          }
      }
+    /**
+     * Gets all the users based on page number and size.
+     * @param users list of users to be filtered.
+     * @param page number.
+     * @param pageSize the number of elements to be retrieved.
+     * @return List of users.
+     */
+    public List<User> arrangeUsersByCreatedDate(final List<User> users, 
+           final Integer page, final Integer pageSize) {
+        List<User> usersList = new ArrayList<User>();
+        Collections.sort(users, new Comparator<User>() {
+            public int compare(final User m1, final User m2) {
+                return m2.getDateJoined().compareTo(m1.getDateJoined());
+            }
+        });
+        if (page != null) {
+            for (int i = page * Constants.PAGE_SIZE; i < page * Constants.PAGE_SIZE + Constants.PAGE_SIZE - 1; i++) {
+                if (i < users.size()) {
+                    usersList.add(users.get(i));
+                }
+                if (pageSize != null && i == pageSize - 1) {
+                    break;
+                }
+            }
+        } else {
+            usersList = users;
+        }
+        return usersList;
+    }
 }
