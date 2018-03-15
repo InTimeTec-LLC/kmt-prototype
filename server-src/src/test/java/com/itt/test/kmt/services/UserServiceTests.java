894b9d05ec12dba6e28ae9863a9be1ebc9c8bde2
ashish <ashish.y@intimetec.com>
2018-03-15 06:08:00 +0530

    update test cases

--- server-src/src/test/java/com/itt/test/kmt/services/UserServiceTests.java
+++ server-src/src/test/java/com/itt/test/kmt/services/UserServiceTests.java
@@ -529,6 +529,27 @@ public class UserServiceTests {
         verify(userRepository, times(1)).save(user);
     }
 
+    @Test()
+    public final void changeUserStatusTestMailFAil() throws MailException, InterruptedException {
+
+        // Arrange
+        User user = testDataRepository.getUsers()
+                .get("user-3");
+        user.setActive(true);
+
+        when(userRepository.findOne(user.getId())).thenReturn(user);
+        when(mailService.sendUserActivateMail(user, false)).thenThrow(new InterruptedException()); 
+        when(userRepository.save(user)).thenReturn(user);
+
+        // Act
+        User updatedUser = userService.changeUserStatus(user.getId(), false);
+
+        // Assert
+        assertEquals(updatedUser.isActive(), false);
+
+        verify(userRepository, times(1)).save(user);
+    }
+    
     @Test(expected = RuntimeException.class)
     public final void activateActiveUser() throws MailException, InterruptedException {
 
