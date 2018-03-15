9c5d014fb23a06e9962ee34d7160a70ae173c06e
ashish <ashish.y@intimetec.com>
2018-03-15 07:07:23 +0530

    remove unused methods

--- server-src/src/main/java/com/itt/kmt/repositories/ArticleRepository.java
+++ server-src/src/main/java/com/itt/kmt/repositories/ArticleRepository.java
@@ -12,34 +12,16 @@ import java.util.List;
 
 
 /**
- * DocumentRepository interface, declares the methods exposed by the repository.
- * Following default methods are provided by CrudRepository and can be used as
- * is without requiring any implementation. save, findOne, exists, findAll,
- * count, delete and deleteAll. Please refer to the javadocs for more details.
+ * ArticleRepository interface, declares the methods exposed by the repository.
+ * that supports pagination and sorting. Following default methods
+ * are provided by PagingAndSortingRepository and can be used as is without requiring
+ * any implementation. findAll(Sort), findAll(Pageable)
  */
-
 public interface ArticleRepository extends PagingAndSortingRepository<Article, String> {
     /**
      * Implements a custom method that returns a pageable Article.
-     * @param createdById id to be searched in createdBy user.
-     * @param page object to get paginated Article list.
-     * @return Article Object matching the search parameter.
-     */
-    @Query("{'createdBy._id': ?0}")
-    Page<Article> findByCreatedBy(ObjectId createdById, Pageable page);
-
-    /**
-     * Implements a custom method that returns a pageable Article.
-     * @param createdById id to be searched in createdBy user.
-     * @param aprroverId id to be searched in approver user.
-     * @param page object to get paginated Article list.
-     * @return Article Object matching the search parameter.
-     */
-    @Query("{'$or':[ {'createdBy._id': ?0}, {'approver._id':?0} ] }")
-    Page<Article> findByCreatedByAndAndApprover(ObjectId createdById, ObjectId aprroverId, Pageable page);
-
-    /**
-     * Implements a custom method that returns a pageable Article.
+     * based on creator of the article, with search on type,status
+     * and search key.
      * @param createdById id to be searched in createdBy user.
      * @param types article type to be searched.
      * @param status , to get article based on article published or unpublished.
@@ -54,8 +36,10 @@ public interface ArticleRepository extends PagingAndSortingRepository<Article, S
 
     /**
      * Implements a custom method that returns a pageable Article.
+     * based on either of the filter (creator/approver) of the article ,
+     * with search on type,status and search key.
      * @param filter , the filter to be applied
-     * @param createdById id to be searched in createdBy user.
+     * @param userId id to be searched in createdBy user.
      * @param types article type to be searched.
      * @param status , to get article based on article published or unpublished.
      * @param search , key to search article.  
@@ -64,25 +48,13 @@ public interface ArticleRepository extends PagingAndSortingRepository<Article, S
      */
     @Query("{'$and':[ {?0: ?1}, { 'articleType._id': { $in: ?2  } }, {approved: { $in: ?3  }}, "
             + "{ 'title' : {$regex:?4, $options: 'i' } }] }")
-    Page<Article> findArticlesByFilter(String filter , ObjectId createdById, List<ObjectId> types,
+    Page<Article> findArticlesByFilter(String filter , ObjectId userId, List<ObjectId> types,
             List<Boolean> status, String search , Pageable page);
 
     /**
-     * Implements a custom method that returns a pageable Article.
-     * @param createdById id to be searched in createdBy user.
-     * @param type article type to be searched.
-     * @param status , to get article based on article published or unpublished.
-     * @param search , key to search article.  
-     * @param page object to get paginated Article list.
-     * @return Article Object matching the search parameter.
-     */
-    @Query("{'$and':[ {'approver._id': ?0}, {'articleType._id': {$regex:?1, $options:'i'}},"
-            + "{approved: ?2}, { 'title' : {$regex:?3, $options:'i'} }] }")
-    Page<Article> findArticlesByApprover(ObjectId createdById, ObjectId type, Boolean status, 
-            String search, Pageable page);
-
-    /**
-     * Implements a custom method that returns a pageable Article.
+     * Implements a custom method that returns a pageable Article. 
+     * based on both the possible filters (creator/approver) of
+     * article , with search on type,status and search key.
      * @param types article type to be searched.
      * @param status , to get article based on article published or unpublished.
      * @param search , key to search article.  
@@ -95,7 +67,9 @@ public interface ArticleRepository extends PagingAndSortingRepository<Article, S
 
     /**
      * Implements a custom method that returns a pageable Article.
-     * @param createdById id to be searched in createdBy user.
+     * based on both the possible filters (creator/approver) of
+     * article , with search on type,status and search key.
+     * @param userId id to be searched in createdBy user.
      * @param type article type to be searched.
      * @param status , to get article based on article published or unpublished.
      * @param search , key to search article.  
@@ -106,16 +80,13 @@ public interface ArticleRepository extends PagingAndSortingRepository<Article, S
             + " { 'title' : {$regex:?3, $options: 'i' } }] }"
             + ", {'$and':[ {'approver._id': ?0}, { 'articleType._id': { $in: ?1  } },"
             + " {approved: { $in: ?2  }}, { 'title' : {$regex:?3, $options: 'i' } }] } ] }")
-    Page<Article> findAllArticles(ObjectId createdById, List<ObjectId> type, List<Boolean> status, String search,
+    Page<Article> findAllArticles(ObjectId userId, List<ObjectId> type, List<Boolean> status, String search,
             Pageable page);
 
 
     /**
-     * Finds a List of Article object that matches the name parameter. Spring
-     * automatically formulates appropriate query depending on the name of the
-     * method. findByXXX() method would look for a match for XXX property and
-     * return the object instance.
-     *
+     * Implements a custom method that returns a pageable Article.
+     * based on search elements , which searches on title, description, and approved status of article.
      * @param search string to be searched in articles.
      * @param page object to get paginated Article list.
      * @param approved approval status.
